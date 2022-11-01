03f385fe56eca730a942a6e55e0d64d4256fe9c2
domsoos <soos.domi@gmail.com>
2022-11-01 11:15:59 -0400

    added folder structure + reuters files

---
+++ sciencealert/sciencealert.py
@@ -0,0 +1,83 @@
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup
+
+filters = ["publishers_list","springer","umi","ebscohost","sciencedirect","emeraldinsight","sagepub","scopusnlai","intechopen","dart-europe","digitool","highwiredoaj",
+"pnas","eprints.nottingham","digital.library.upenn.edu/books/","etd.ohiolink","escholarship","lib","ieeexplore","acm","wiley","sciencedirect",
+"acs","aiaa","aip","ajpe","aps","ascelibrary","asm","asme","bioone","birpublications","bmj","emeraldinsight","geoscienceworld","icevirtuallibrary","informs","ingentaconnect",
+"iop","jamanetwork","joponline","jstor","mitpressjournals","nrcresearchpress","oxfordjournals","royalsociety","rsc","rubberchemtechnol","sagepub","scientific","spiedigitallibrary","tandfonline","theiet",
+"doi", "arxiv","eurekalert"] #"article", "journal","science", ".gov", ".org", "frontiersin"]
+rejectfilters = ["thetimes","futurity", "news","press-release" ,"nytimes", "reuters", "magazine", "mevzuat", "gov.uk", "scientificamerican", "energy.gov", "wikipedia", "cdc.gov", "planalto.gov", "planetary.org", "/maps/"]
+reject_count = 0
+evidence_count = 0
+
+with open("sciencealertarticles.csv") as file:
+	reader = csv.reader(file, delimiter=',')
+	header = next(reader)
+	#,title,category,author,url,plaintext,html
+
+	article_id = 0
+	dictionary = []
+	for row in reader:
+		article_id += 1
+		# remove linebreaks
+		plain_text = row[5].replace("<linebreak>","")
+		html = row[6]
+		hrefs = []
+		evidence = []
+
+
+		soup = BeautifulSoup(html, "html.parser")
+		#print(soup)
+
+		for link in soup.find_all('a', href=True):
+			hrefs.append(str(link.get('href')))
+		
+
+		# iterate thru all links and filter out the ones that has strings that could potentially be evidence
+		for url in hrefs:
+			for strs in filters:
+				if strs in url:
+					if url in evidence:
+						break
+					if "eurekalert" in url:
+						evidence.append(url)
+						continue
+					if strs == "science":
+						if "sciencealert" in url or "livescience" in url:
+							reject_count+=1
+							break
+					else:
+						for reject in rejectfilters:
+							if reject in url or url[len(url)-5:] == ".org/" or url[0] == "/":
+								reject_count+=1
+								break
+						else:
+							evidence.append(url)
+							evidence_count += 1
+
+		eid = 0
+		for url in evidence:
+			eid+=1
+			data = {
+				'id': article_id,
+	            'title': row[1],
+	            'category': row[2],
+	            'author': row[3],
+	            'url': row[4],
+	            'plaintext': plain_text,
+	            'keep/filter': '  ',
+	            'evidence_id': eid,
+	            'evidence': url,
+	        }
+
+			dictionary.append(data)
+
+
+print("total evidence: ", evidence_count)
+print("total reject: ",reject_count)
+df_articles = pd.DataFrame(dictionary)
+df_articles.to_csv('evidencetofilter.csv', encoding='utf-8-sig')
+
+
+
---
+++ true/reuterscrapper.py
@@ -0,0 +1,137 @@
+# pip install semanticscholar
+# pip install newspaper3k
+# pip install summa
+# pip install pattern
+from bs4 import BeautifulSoup
+import requests
+import urllib.request,sys,time
+import re
+import pandas as pd
+from newspaper import Article
+import json
+#from summa import keywords
+import nltk
+import pattern as patt
+from semanticscholar import SemanticScholar
+import htmlmin  
+
+def requestpage(URL):
+    try:
+
+        # this might throw an exception if something goes wrong.
+        page=requests.get(URL) 
+        # this describes what to do if an exception is thrown 
+    except Exception as e:    
+  
+        # get the exception information
+        error_type, error_obj, error_info = sys.exc_info()      
+        
+        #print the link that cause the problem
+        print ('ERROR FOR LINK:', URL)
+        
+        #print error info and line that threw the exception                          
+        print (error_type, 'Line:', error_info.tb_lineno)
+    return page
+
+list_articles_url = []
+
+
+count = 1
+article_count = 0
+title_list = []
+while len(list_articles_url) < 3200:
+
+    URL = 'https://www.reuters.com/news/archive/technologyNews?view=page&page='+str(count)+'&pageSize=10'
+    count += 1
+    page = requestpage(URL)
+    soup = BeautifulSoup(page.text, "html.parser")
+    
+    
+    if len(soup.find_all(attrs={'class': 'news-headline-list'})) == 0:
+        break
+
+    articles_archives = soup.find_all(attrs={'class': 'news-headline-list'})[0]
+
+    articles = articles_archives.find_all('article')
+
+    for article in articles:
+        a_tag = article.find('a')
+
+        # handle duplication
+        newtag = a_tag.attrs['href'].split('-')
+        # id number is the last element in url
+        newtag.pop(-1)
+        newtag = "-".join(str(x) for x in newtag)
+        #print(newtag)
+
+        if newtag not in title_list:
+            title_list.append(newtag)
+            list_articles_url.append(a_tag.attrs['href'])
+            article_count += 1
+        
+    if article_count % 100 == 0:
+        title_list = []
+        print(article_count, " articles extracted so far")
+
+
+print(len(list_articles_url))
+print("end of first loop")
+reuters_data_list = []
+
+count = 0
+
+for item in list_articles_url:
+    try:
+        url = 'https://www.reuters.com/news' + item
+        #print(url)
+
+        page = requestpage(url)
+        soup = BeautifulSoup(page.text, "html.parser")
+        title = soup.find("meta", {'property':'og:title'}).attrs['content']
+        claim = soup.find("meta", {'property':'og:description'}).attrs['content']
+        time = soup.find("meta", {'property':'og:article:published_time'}).attrs['content']
+
+        # scrape all links
+        all_links = []
+        href_tags = soup.find_all(href=True)
+        for tag in href_tags:
+            href = tag.get('href')
+            if href[0] == '/':
+                href = url + href
+            all_links.append(href)
+
+        data = ''
+        content = ""
+        for data in soup.find_all('p'):
+            content += data.get_text()
+
+        #print()
+
+        reuters_data = {
+            'title': title,
+            'claim': claim,
+            'time': time,
+            'url': url,
+            'plaintext': content[10:],
+            'all_links':all_links,
+        }
+
+        if reuters_data:
+            #print(reuters_data,"\n")
+            reuters_data_list.append(reuters_data)
+            count += 1
+            if count % 50 == 0:
+                print(count, " articles done")
+
+    except Exception as e:
+        # get the exception information
+        error_type, error_obj, error_info = sys.exc_info()      
+        
+        #print error info and line that threw the exception                          
+        print (error_type, 'Line:', error_info.tb_lineno)
+        print (e)
+
+
+print("number of articles: ", count)
+df_articles = pd.DataFrame(reuters_data_list)
+df_articles.to_csv('reutersarticles.csv', encoding='utf-8-sig')
